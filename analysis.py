73c8d7741b58054d8784c315b246942012be9700
idsyr <idsyr@nus.edu.sg>
2018-04-11 15:30:11 +0800

    Start implementing sentiment analyser using pyspark.

---
+++ analysis.py
@@ -0,0 +1,29 @@
+from pyspark.ml.feature import RegexTokenizer, HashingTF, IDF, CountVectorizer, Normalizer
+from pyspark.sql.functions import col, udf
+from pyspark.sql.types import IntegerType, DoubleType, StringType, StructType, StructField
+from pyspark.sql import SparkSession
+from pyspark.ml.feature import StopWordsRemover
+
+
+if __name__ == "__main__":
+    data_file = r"file:///root/spark_repos_ruizhi/distributed-sentiment-analysis-on-twitter-data/twitter_scraper/data/twitter_data.csv"
+
+    # Initialize a Spark session
+    spark = SparkSession \
+        .builder \
+        .appName("SentimentAnalysis") \
+        .getOrCreate()
+
+    # define the data schema(format/structure) for our twitter data in the csv file
+    twitter_data_schema = StructType([StructField("date_str", StringType(), True),
+                                      StructField("text", StringType(), True),
+                                      StructField("user_id", StringType(), True),
+                                      StructField("location", StringType(), True)])
+
+    df_raw = spark.read.csv(
+        data_file, schema=twitter_data_schema
+    )
+
+    df_raw.show(truncate=False)
+
+    spark.stop()
